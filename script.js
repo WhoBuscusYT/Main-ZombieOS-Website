7b57e9d65263aa6361da7157dc196e3c04e3f81a
WhoBuscusYT <jj987531t@gmail.com>
2026-05-30 18:06:21 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+/* PLATFORM DETECTION */
+
+const platformText =
+document.getElementById("platform-text");
+
+const downloadContainer =
+document.getElementById("download-container");
+
+if(platformText){
+
+const platform =
+navigator.platform.toLowerCase();
+
+if(platform.includes("win")){
+
+platformText.textContent =
+"Windows detected. Your system supports the latest version of ZOS.";
+
+}
+
+else{
+
+platformText.innerHTML =
+`
+<div class="unsupported-warning">
+Your OS doesn't support the latest version of ZOS.
+</div>
+`;
+
+downloadContainer.innerHTML = "";
+
+}
+
+}
+
+/* YEAR */
+
+const year =
+document.getElementById("year");
+
+if(year){
+
+year.textContent =
+new Date().getFullYear();
+
+}
