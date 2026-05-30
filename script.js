a7b50fa213c350cb875a22d902d1d5cbc0a1c9b0
WhoBuscusYT <jj987531t@gmail.com>
2026-05-30 11:48:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 const shellMessages = [
 
 `> boot ZombieOS
@@ -69,3 +67,47 @@ typeMessage();
 
 animateShell("shell-text");
 animateShell("shell-text-2");
+
+/* =========================
+   FIREBASE LOGIN DETECTION
+========================= */
+
+import("https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js")
+.then(async ({ initializeApp }) => {
+
+const { getAuth, onAuthStateChanged } =
+await import("https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js");
+
+const firebaseConfig = {
+apiKey: "AIzaSyDG0hSabeqYdGgSISOgvSnkOwATXDLiV9g",
+authDomain: "zombieos.firebaseapp.com",
+projectId: "zombieos",
+storageBucket: "zombieos.firebasestorage.app",
+messagingSenderId: "577624378484",
+appId: "1:577624378484:web:3e88e693724bde8e89d521",
+measurementId: "G-LV0T97LGWP"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+
+const loginLinks =
+document.querySelectorAll('a[href="login.html"]');
+
+if(user){
+
+loginLinks.forEach(link => {
+
+link.textContent = "Dashboard";
+link.href = "dashboard/index.html";
+
+});
+
+}
+
+});
+
+});
