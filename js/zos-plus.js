d3b3a2ae158bb4688f47fa240f42b68de000d5c8
WhoBuscusYT <jj987531t@gmail.com>
2026-06-01 00:19:04 -0400

    Create zos-plus.js

---
+++ js/zos-plus.js
@@ -0,0 +1,35 @@
+const basicButton =
+document.getElementById(
+"basic-buy-button"
+);
+
+const premiumButton =
+document.getElementById(
+"premium-buy-button"
+);
+
+if(basicButton){
+
+basicButton.onclick =
+()=>{
+
+alert(
+"ZOS+ subscriptions are coming soon."
+);
+
+};
+
+}
+
+if(premiumButton){
+
+premiumButton.onclick =
+()=>{
+
+alert(
+"ZOS+ subscriptions are coming soon."
+);
+
+};
+
+}
